c812de8fe16c8e67863ff89e0a7fe03e709cc1aa
Danya Alexeyevsky <dendik@mouse>
2013-12-27 20:23:06 +0400

    utils: added `logging`, a smart decorator for logging function calls

--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
 import sys
+import functools
 
 def log(*args):
 	sys.stderr.write(" ".join(map(str, args)) + "\n")
+
+def logging(message):
+	def decorator(f):
+		@functools.wraps(f)
+		def result(*args, **kws):
+			log(message)
+			return f(*args, **kws)
+		return result
+
+	if isinstance(message, str) or isinstance(message, unicode):
+		return decorator
+	else:
+		f, message = message, (message.__name__ + "...")
+		return decorator(f)
