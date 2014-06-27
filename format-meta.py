29faf80d12fce4e6a4aa7af0c1a695d2155e0cee
Danya Alexeyevsky <6286749+dendik@users.noreply.github.com>
2014-06-27 19:46:53 +0400

    format-meta: write separate scales file too

--- format-meta.py
+++ format-meta.py
@@ -1,5 +1,7 @@
 import czifile
 import optparse
+import os
+import sys
 from processor import print_czi_metadata
 
 def print_scale(filename):
@@ -12,7 +14,15 @@ def print_scale(filename):
 
 if __name__ == "__main__":
 	p = optparse.OptionParser()
+	p.add_option('-w', '--write', default='scale.csv')
 	options, args = p.parse_args()
 
 	for filename in args:
 			print_scale(filename)
+
+			if options.write:
+				ofn = os.path.join(os.path.dirname(filename), options.write)
+				with open(ofn, 'w') as fd:
+					backup, sys.stdout = sys.stdout, fd
+					print_scale(filename)
+				sys.stdout = backup
