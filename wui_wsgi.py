6fa670e65dbc93f4f28f25ebcb7ee1660d9d45fe
Danya Alexeyevsky <dendik@mouse>
2017-06-16 14:54:49 +0300

    WSGI: added template for virtualenv activation

--- wui_wsgi.py
+++ wui_wsgi.py
@@ -2,4 +2,8 @@ import sys
 from os.path import dirname
 sys.path.append(dirname(__file__))
 
+## If you are using virtualenv, uncomment the following lines:
+# virtualenv = '...path-to.../bin/activate_this.py'
+# execfile(virtualenv, dict(__file__=virtualenv))
+
 from wui import app as application
